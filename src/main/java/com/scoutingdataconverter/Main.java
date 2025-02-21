6eb449a531c62bc1ae028d040a58847e978abb25
agent965 <aiden.rothstein@gmail.com>
2025-02-21 12:38:56 -0800

    .

--- src/main/java/com/scoutingdataconverter/Main.java
+++ src/main/java/com/scoutingdataconverter/Main.java
@@ -1,14 +1,19 @@
 package com.scoutingdataconverter;
 
+import java.io.IOException;
+
+import com.google.zxing.NotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, NotFoundException{
+        System.out.println(com.google.zxing.NotFoundException.getNotFoundInstance());
         System.out.println("Hello world!");
         String filepath = "QRTest.png";
         try {
             String qrText = QRCodeReader1.readQrCode(filepath);
             System.out.println(qrText);
-        } catch (Exception e) {
-
+        } catch (NotFoundException e) {
+            System.out.println("Error reading QR code: " + e);
         }
         
         
