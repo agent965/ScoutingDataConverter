e762da33162567142c41bf070b8344e9a5570c0f
agent965 <aiden.rothstein@gmail.com>
2025-03-06 19:25:23 -0800

    .

--- src/main/java/com/scoutingdataconverter/Cam.java
+++
@@ -1,4 +0,0 @@
-package com.scoutingdataconverter;
-public class Cam {
-    
-}
---
+++ src/main/java/com/scoutingdataconverter/Camera.java
@@ -0,0 +1,22 @@
+package com.scoutingdataconverter;
+
+import java.io.IOException;
+
+import com.google.zxing.NotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NotFoundException{
+        System.out.println(com.google.zxing.NotFoundException.class.getName());
+        System.out.println("Hello world!");
+        String filepath = "QRTest.png";
+        try {
+            String qrText = QRCodeReader1.readQrCode(filepath);
+            System.out.println(qrText);
+        } catch (NotFoundException e) {
+            System.out.println("Error reading QR code: " + e);
+        }
+        
+        
+        
+    }
+}
